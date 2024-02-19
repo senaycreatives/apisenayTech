54a74cc1ac2caeec8ec92e0aaf57196a63345dc2
andi-gech <andi.fab23@gmail.com>
2024-02-19 02:43:24 -0800

    update remove socket

--- index.js
+++ index.js
@@ -1,88 +1,51 @@
 const bott = '6795594212:AAH_HoH1rlhQ_u4xsnkRp0o2NorK46YZ6CY';
+
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const TelegramBot = require('node-telegram-bot-api');
 
 const app = express();
-const cors = require('cors');
-
-app.use(cors());
-  
-
-const server = http.createServer(app);
-const io = socketIo(server);
-
 const PORT = process.env.PORT || 3300;
 
+app.use(cors());
+app.use(bodyParser.json());
 
-const telegramGroupChatId = '-891865410'; 
-
+const telegramGroupChatId = '-891865410';
 
 const bot = new TelegramBot(bott, { polling: true });
 
+app.post('/message', (req, res) => {
+    const { message } = req.body;
+    console.log(message);
 
-const userStates = {};
+    const messageToSend = `
+        Hey buddy, we have *New Customer*:
 
+        *What kind of website/app/bot do you like us to build?*:
+        *${message.whatDoYouLike}*
 
-io.on('connection', (socket) => {
-    console.log(`Client connected with socket ID: ${socket.id}`);
+        *Give me your Email Address*:
+        *${message.email}*
 
-   
-    socket.emit('message', 'Hello! Welcome to the chat.');
-    socket.emit('message', 'What kind of website do you like?');
-  
-    const questions = [
-        'What kind of website/app/bot do you like us to build?',
-        'Give me your Email Adress?',
-        'Who do we call you?',
-        'Any additional info To Tell?'
-    ];
-    let currentStep = 0;
-    let userData = {};
+        *Who do we call you?*:
+        *${message.caller}*
 
-    socket.on('message', (message) => {
-     console.log(message);
-        userData[questions[currentStep]] = message;
+        *Any additional info to tell?*:
+        *${message.additionalInfo}*
+    `;
 
-       
-        currentStep++;
-
-        if (currentStep < questions.length) {
-            socket.emit('message', questions[currentStep]);
-        } else {
-           
-const messageToSend = `
-Hey buddy, we have *New Customer*:
-
-*What kind of website/app/bot do you like us to build?*:
-*${userData[questions[0]]}*
-
-*Give me your Email Address*:
-*${userData[questions[1]]}*
-
-*Who do we call you?*:
-*${userData[questions[2]]}*
-
-*Any additional info to tell?*:
-*${userData[questions[3]]}*
-`;
-
-
-bot.sendMessage(telegramGroupChatId, messageToSend, { parse_mode: 'Markdown' });
-socket.emit('message', 'Thank you! Your message has been sent wait for an email response.');
-            
-            currentStep = 0;
-            userData = {};
-        }
-    });
-
-    socket.on('disconnect', () => {
-        console.log(`Client with socket ID ${socket.id} disconnected.`);
-    });
+    bot.sendMessage(telegramGroupChatId, messageToSend, { parse_mode: 'Markdown' })
+        .then(() => {
+            res.status(200).json({ message: 'Message sent successfully!' });
+        
+        })
+        .catch((error) => {
+            console.error('Error sending message to Telegram:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
